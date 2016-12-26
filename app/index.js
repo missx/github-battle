028a95021913c61402723cd2c9991a828a57a8a1
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-26 18:37:27 -0300

    Add routes

--- app/index.js
+++ app/index.js
@@ -1,49 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-    render: function() {
-        return (
-            <img src={this.props.imageURL} style={{height: 100, width: 100}}/>
-        )
-    }
-});
-
-var ProfileLink = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <a href={"https://www.github.com/" + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function() {
-        return (
-            <div>
-                {this.props.name}
-            </div>
-        )
-    }
-});
-
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.imageUrl}/>
-                <ProfileName name={this.props.name}/>
-                <ProfileLink username={this.props.username}/>
-            </div>
-        )
-    }
-});
+var Routes = require('./config/routes');
 
 ReactDOM.render(
-    <Avatar name="Ximena Lasserre" imageUrl="http://images.gr-assets.com/users/1311714506p6/5378745.jpg" username="missx"/>,
+    Routes,
     document.getElementById('app')
 );
