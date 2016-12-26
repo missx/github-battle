bb1204f23d40a42a0a2861e5f9be9889573aae13
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-26 17:39:51 -0300

    Pure functions. f(d) = v. Props and nesting

--- app/index.js
+++ app/index.js
@@ -1,15 +1,49 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
     render: function() {
         return (
-            <div>Hello World</div>
+            <img src={this.props.imageURL} style={{height: 100, width: 100}}/>
+        )
+    }
+});
+
+var ProfileLink = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <a href={"https://www.github.com/" + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
+        )
+    }
+});
+
+var ProfileName = React.createClass({
+    render: function() {
+        return (
+            <div>
+                {this.props.name}
+            </div>
+        )
+    }
+});
+
+var Avatar = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <ProfilePic imageUrl={this.props.imageUrl}/>
+                <ProfileName name={this.props.name}/>
+                <ProfileLink username={this.props.username}/>
+            </div>
         )
     }
 });
 
 ReactDOM.render(
-    <HelloWorld/>,
+    <Avatar name="Ximena Lasserre" imageUrl="http://images.gr-assets.com/users/1311714506p6/5378745.jpg" username="missx"/>,
     document.getElementById('app')
 );
