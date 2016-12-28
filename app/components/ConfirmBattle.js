6cc154c69146c51109f2bd52bf0965b166d9a588
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-28 19:02:42 -0300

    Axios, promises, github api

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,12 @@
 var React = require('react');
 
+var puke = function(obj) {
+    return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+}
 function ConfirmBattle(props) {
     return props.isLoading === true
         ? <p> Loading! </p>
-        : <p> Confirm Battle </p>
+        : <p> Confirm Battle: {puke(props)}</p>
 }
 
 module.exports = ConfirmBattle;
