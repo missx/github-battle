51ead1b7b7497e888bef981ed9324f1985eaddd0
Ximena Lasserre <ximena.lasserre@modelit.xyz>
2016-12-30 17:25:59 -0300

    Highly reusable component

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -6,10 +6,11 @@ var styles = require('../styles');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var UserDetails = require('./UserDetails');
 var MainContainer = require('./MainContainer');
+var Loading = require('./Loading');
 
-function ConfirmBattle(props) {
+function ConfirmBattle (props) {
     return props.isLoading === true
-        ? <p> Loading! </p>
+        ? <Loading speed={500} text='Waiting'/>
         : <MainContainer>
             <h1>Confirm Players</h1>
             <div className='col-sm-8 col-sm-offset-2'>
